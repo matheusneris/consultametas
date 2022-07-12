b15f330917cd7526a30c7a8b27ac61c9828ca3db
Matheus Neris <matheusneris7@gmail.com>
2022-07-12 20:55:36 -0300

    Primeira versao, estrutura basica

---
+++ backend/src/test/java/com/devmatheus/metas/MetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmatheus.metas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
